d866a5d43ca7f2785e6e50b2af8b1fa0f5f7c7b4
theogirl <theogirl@outlook.com>
2015-05-04 22:14:05 -0400

    font-awesome added

--- js/todo-script.js
+++ js/todo-script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 // User enters text in input field and presses keyboard Return
 
+	$('input').focus();
+
 	$('input').keydown(function(event) {
 		if (event.which == 13) {
 		var value = $(this).val();
@@ -9,7 +11,7 @@ $(document).ready(function() {
 				alert('Please enter a list item!');
 			}
 			else {
-			$('.list ul').append("<li class='todos'>" + "<button>Not Done</button>" + ' ' + value + "<div class='delete'>Delete Me</div>" + "</li>");
+			$('.list ul').append("<li class='todos'>" + "<button>Not Done</button>" + ' ' + value + "<div class='delete'>" + "<i class='fa fa-trash-o'></i>" + "</div>" + "</li>");
 			$(this).val(''); //passes an empty string to input field
 			$(this).focus(); //returns the focus to this field
 			}
@@ -24,7 +26,7 @@ $(document).ready(function() {
 			alert('Please enter a list item!');
 		}
 		else {
-		$('.list ul').append("<li class='todos'>" + "<button>Not Done</button>" + ' ' + value + "<div class='delete'>Delete Me</div>" + "</li>");
+		$('.list ul').append("<li class='todos'>" + "<button>Not Done</button>" + ' ' + value + "<div class='delete'>" + "<i class='fa fa-trash-o'></i>" + "</div>" + "</li>");
 		$('input').val(''); //passes an empty string to input field
 		$('input').focus(); //returns the focus to this field
 		}
